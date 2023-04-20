8a5b543823c0d958c76e82649c450695298b2cad
andreskps <90162367+andreskps@users.noreply.github.com>
2023-04-20 09:36:34 -0500

    clase carro

---
+++ carro.js
@@ -0,0 +1,12 @@
+class carro{
+    constructor(marca, modelo, color, kilometraje){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.kilometraje = kilometraje;
+    }
+
+    mostrar(){
+        return `Hola, soy un ${this.marca} ${this.modelo} ${this.color} y tengo ${this.kilometraje} kms`;
+    }
+}

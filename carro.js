836cccb550a6506084467ebb88027374209ce4d3
andreskps <90162367+andreskps@users.noreply.github.com>
2023-04-20 11:14:24 -0500

    atributo pais clase carro

--- carro.js
+++ carro.js
@@ -1,9 +1,10 @@
 class carro{
-    constructor(marca, modelo, color, kilometraje){
+    constructor(marca, modelo, color, kilometraje,pais){
         this.marca = marca;
         this.modelo = modelo;
         this.color = color;
         this.kilometraje = kilometraje;
+        this.pais = pais;
     }
 
     mostrar(){
@@ -21,4 +22,9 @@ class carro{
     getColor(){
         return this.color;
     }
+
+    getPais(){
+        return this.pais;
+    }
+
 }

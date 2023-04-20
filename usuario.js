a94984853063104271f241ec1799a2c9fd1b1650
andreskps <90162367+andreskps@users.noreply.github.com>
2023-04-20 09:12:39 -0500

    clase usuario

---
+++ usuario.js
@@ -0,0 +1,15 @@
+class user{
+    
+    constructor(){
+        this.name = 'Juan';
+        this.lastname = 'Perez';
+    }
+
+    getFullName(){
+        return this.name + ' ' + this.lastname;
+    }
+
+    getAge(){
+        return 20;
+    }
+}

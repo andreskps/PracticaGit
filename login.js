7a36ec570b80b8c328cbb74aa4c7708264176aa6
andreskps <pipesierra146@gmail.com>
2023-04-23 12:27:41 -0500

    correcion bug login

--- login.js
+++ login.js
@@ -2,8 +2,8 @@ class Login{
     constructor(){
         this.email = document.getElementById('email');
         this.password = document.getElementById('password');
-        this.loginButton = document.getElementById('loginButton');
-        this.loginButton.addEventListener('click', this.login.bind(this));
+        this.loginButton = document.getElementById('button');
+        this.loginButton.addEventListener('click', this.login);
     }
 
     login(){
@@ -14,7 +14,7 @@ class Login{
             // Signed in 
             // ...
             console.log(user);
-            window.location.href = "index.html";
+            window.location.href = "home.html";
         })
         .catch((error) => {
             var errorCode = error.code;
